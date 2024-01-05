f4fea9ca50d6bfb4fab5066ea8a69cea72f51dac
박서영 <seokachuu@gmail.com>
2024-01-06 00:10:39 +0900

    위로가기버튼 추가

--- main.js
+++ main.js
@@ -87,3 +87,29 @@ function showMovies(data) {
     }
 
 }
+
+
+
+/* scroll-top */
+const btt = document.querySelector('.go-top');
+let scrollAmt = 0;
+
+window.addEventListener('scroll',function(){
+  scrollAmt = window.scrollY;
+  console.log(scrollAmt);
+
+  if(scrollAmt > 300){
+    btt.style.display = "block";
+  }else{
+    btt.style.display = "none";
+  }
+});
+
+
+btt.addEventListener('click',function(){
+  window.scrollTo({
+    top:0,
+    left:0,
+    behavior:'smooth'
+  })
+});
